7b12577641ebea8eb2e7be270083b2020d6028c0
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-05-08 07:20:31 +0530

    Password encoding added

--- src/main/java/lk/acpt/notecollectorappapi/service/impl/UserServiceIMPL.java
+++ src/main/java/lk/acpt/notecollectorappapi/service/impl/UserServiceIMPL.java
@@ -10,6 +10,7 @@ import lk.acpt.notecollectorappapi.repo.UserRepo;
 import lk.acpt.notecollectorappapi.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -28,6 +29,8 @@ public class UserServiceIMPL implements UserService {
         if (userRepo.existsByEmailAddress(saveUserDTO.getEmailAddress())) {
             throw new DuplicateException("Email address already exists");
         }
+        String encodedPassword = new BCryptPasswordEncoder().encode(saveUserDTO.getPassword());
+        saveUserDTO.setPassword(encodedPassword);
         User user = modelMapper.map(saveUserDTO,User.class);
         userRepo.save(user);
         return user;
@@ -38,7 +41,7 @@ public class UserServiceIMPL implements UserService {
         Optional<User> optionalUser = userRepo.findByEmailAddress(logInDTO.getEmailAddress());
         if (optionalUser.isPresent()){
             User dbUser = optionalUser.get();
-            if (logInDTO.getPassword().equals(dbUser.getPassword())) {
+            if (new BCryptPasswordEncoder().matches(logInDTO.getPassword(), dbUser.getPassword())) {
                 return dbUser;
             } else {
                 throw new UnauthorizedException("Unauthorized user");
